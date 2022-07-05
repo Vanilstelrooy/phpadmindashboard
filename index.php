56263b31b1387c09cda7f30b2b9d84d1c418eb9b
Vanilstelrooy <vanistelrooyobare@gmail.com>
2022-07-05 13:25:00 +0300

    added logics and includes

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+   session_start();
+?>
+<!DOCTYPE html>
+<html>
+ <?php require_once("includes/headers.php") ?> 
+<body>
+	<!-- All our code. write here   -->
+	<?php require_once("includes/navbar.php") ?>
+	<div class="sidebar">
+	<?php require_once("includes/sidebar.php") ?>
+	</div>
+	<div class="maincontent">
+        <div class="container-fluid">
+            <div class="row">
+				<div class="col-lg-12">
+					<div class="card-header bg-dark text-white text-center">
+						<span>Top Content</span>
+					</div>
+				</div>
+				<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-3">
+						<div class="card-header bg-dark text-white text-center">
+							<span>Students</span>
+						</div>
+						<div class="card-body">
+							<span><i class="fa fa-group fa-3x"></i></span>
+							<span class="float-right">00</span>
+						</div>
+						<div class="card-footer"></div>
+					</div>
+					<div class="col-lg-3">
+						<div class="card-header bg-dark text-white text-center">
+							<span>Available Courses</span>
+						</div>
+						<div class="card-body">
+							<span><i class="fa fa-folder-open fa-3x"></i></span>
+							<span class="float-right">00</span>
+						</div>
+						<div class="card-footer"></div>
+					</div>
+					<div class="col-lg-3">
+						<div class="card-header bg-dark text-white text-center">
+							<span>Campuses</span>
+						</div>
+						<div class="card-body">
+							<span><i class="fa fa-graduation-cap fa-3x"></i></span>
+							<span class="float-right">00</span>
+						</div>
+						<div class="card-footer"></div>
+					</div>
+					<div class="col-lg-3">
+						<div class="card-header bg-dark text-white text-center">
+							<span>Users</span>
+						</div>
+                        <div class="card-body">
+							<span><i class="fa fa-group fa-3x"></i></span>
+							<span class="float-right">00</span>
+						</div>
+						<div class="card-footer"></div>
+					</div>
+					<div class="col-lg-12">
+						<div class="card-header bg-dark text-white text-center">
+							<span>Student Analysis</span>
+						</div>
+					</div>
+					<div class="container-fluid">
+					<div class="body-shadow">
+						<span><i class="fa fa-line-chart fa-4x"></i></span>
+					</div>
+					</div>
+				</div>
+			    </div>
+			</div>
+		</div>
+	</div>
+
+	
+    <?php require_once("includes/scripts.php") ?>
+</body>
+</html>
