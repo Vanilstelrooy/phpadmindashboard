2c43159ab3fc6238d988e8abd22de96d39ec39d9
Vanilstelrooy <vanistelrooyobare@gmail.com>
2022-07-07 13:44:44 +0300

    More on edit button

--- edit-enrollment.php
+++ edit-enrollment.php
@@ -4,8 +4,11 @@
     while($fetchuser = mysqli_fetch_array($queryuser))
     {
         $fullname = $fetchuser['fullname'];
+        $phone = $fetchuser['phonenumber'];
+        $email = $fetchuser['email'];
+        $gender = $fetchuser['gender'];
+        $course = $fetchuser['course'];
     }
-
 
    session_start();
 ?>
@@ -24,7 +27,48 @@
              <div class="col-lg-12">
                 <div class="card">
                     <div class="card-header bg-dark text-center text-white">
-                        <h4 >Edit Student :<?php echo $fullname?></h4>
+                        <h4 >Edit Student </h4>
+                    </div>
+                    <div class="card-body">
+                    <form action="enroll.php" method="POST">       
+        <div class="row">
+                <div class="mb-3 col-lg-6">
+                    <label for="fullname" class="form-label">Full Name</label>
+                    <input type="text" name="fullname" class="form-control" value="<?php echo $fullname?>" placeholder="Enter Your Full Name">
+                </div>
+                <div class="mb-3 col-lg-6">
+                    <label for="phone" class="form-label">Phone Number</label>
+                    <input type="tel" name="phone" class="form-control"value="<?php echo $phone?>"  placeholder="Enter Your Phone Number">
+                </div>
+        </div> 
+        <div class="row">
+                <div class="mb-3 col-lg-6">
+                    <label for="email" class="form-label">Email Address</label>
+                    <input type="email" name="email" class="form-control" value="<?php echo $email?>"  placeholder="Enter Your Email Address">
+                </div>
+                <div class="mb-3 col-lg-6">
+                    <label for="gender" class="form-label">Whats your gender?</label><br>
+                    <select name="gender"  aria-label="Default select example" class="form-control" >
+                        <option  selected><?php echo $gender?></option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                    </select>
+                </div>
+        </div>
+        <div class="row">
+                <div class="mb-3 col-lg-6">
+                    <label for="course"class="form-label ">What's your prefered course?</label><br>
+                    <select name="course" aria-label="Default select example" class="form-control">
+                        <option selected><?php echo $course?></option>
+                        <option value="Android App Development">Android App Development</option>
+                        <option value="Web Design & Development">Web Design & Development</option>
+                        <option value="Data Analysis">Data Analysis</option>
+                        <option value="Cyber Security">Cyber Security</option>
+                    </select>
+                </div>
+        </div>      
+        <button type="submit"name="submitButton" class="btn btn-outline-primary" >Update Record</button>
+      </form> 
                     </div>
                 </div>
              </div>
